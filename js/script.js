d3d7dee4eb4763af478acc5d6209d86cf3e5af0e
Dahwin DelaCruz <dahwin.delacruz@gmail.com>
2022-02-23 16:09:53 -0800

    Assignment complete

---
+++ js/script.js
@@ -0,0 +1,177 @@
+//STEP 1
+
+//let someMonth;
+//function theMonth();
+//let currentMonth;
+//let summerMonth;
+//function myLibrary();
+
+
+
+//STEP 2
+
+//let numericLiteral = 6;
+//let stringLiteral = "Hello World!";
+//let booleanLiteral = true;
+//let nullLiteral = null;
+
+
+
+//STEP 3
+
+//let complexExpression = 6 * (3 + 1) + 8;
+//let variableExpression = 7 + (10 - 5);
+
+
+
+//STEP 4
+
+//let firstName;
+//let lastName;
+//let address;
+//let city;
+//let state;
+//let zipCode;
+//let yourAge;
+//let referralSource;
+//let mayWeContactYou;
+
+
+
+//STEP 5
+
+//firstName = 'Dahwin';
+//lastName = 'DelaCruz';
+//address = 'Fenton Pkwy';
+
+
+
+//STEP 6
+
+//let areaCode = ('I live in the " + 858 + " area code.');
+//console.log (areaCode);
+
+
+
+//STEP 7
+
+//let firstVariable = true + ' is my age';
+//console.log(firstVariable);
+
+//let secondVariable = 40 + false;
+//console.log(secondVariable);
+
+
+
+//STEP 8
+
+//let someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\"";
+//console.log(someString);
+
+
+
+//STEP 9
+
+//let nullValue = null;
+//console.log(nullValue);
+
+//let undefinedValue;
+//console.log(undefinedValue);
+
+
+
+//STEP 10
+
+//typeof "DelaCruz"
+//typeof 19
+//typeof true
+//typeof new Date()
+//typeof myShorts
+
+
+
+//STEP 11
+
+//alert("Hello " + "Dahwin" + ", welcome to the Javascript class!");
+
+
+
+//STEP 12
+
+//let name  = 'Dahwin';
+//alert("Hello " + name + ", welcome to the Javascript class!");
+
+
+
+//STEP 13
+
+//let name  = 'Dahwin';
+//let course = 'Javascript';
+//alert("Hello " + name + ", welcome to the " + course + " class!");
+
+
+
+//STEP 14
+
+//let name  = 'Dahwin';
+//let course = 'Javascript';
+//alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+
+
+
+//STEP 15
+
+//let name  = window.prompt('Please enter your name.');
+//let course = 'Javascript';
+//alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+
+
+
+//STEP 16
+
+//let name  = window.prompt('Please enter your name.');
+//let course = window.prompt('Please enter the class you are taking.');
+//alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+
+
+
+//STEP 17
+
+//let x = 10;
+//let y = 20;
+//console.log(x + y);
+
+
+
+//STEP 18
+
+//let x = 20;
+//console.log(x + 20);
+
+
+
+//STEP 19
+
+//let x = 20;
+//console.log(x * 5);
+
+
+
+//STEP 20
+
+//let x = 20 % 3;
+//console.log( x / 1);
+
+
+//STEP 21
+
+//let x = 6000;
+//let y = 6000;
+//console.log(x == y);
+
+
+//STEP 22
+
+//let x = 6000;
+//let y = 6000;
+//console.log(x != y);
